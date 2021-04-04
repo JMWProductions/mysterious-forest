c5197ce3a5b135140677afc908d925d037347c54
Wojciech Olejnik <divoolej@gmail.com>
2021-04-04 19:40:49 +0200

    Fix sound

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -14,17 +14,17 @@ var cooldown
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	rock = preload("../scenes/Rock.tscn")
-	rock_counter = 3
-	cooldown = 0
+  rock = preload("../scenes/Rock.tscn")
+  rock_counter = 3
+  cooldown = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	manage_velocity(delta)
-	manage_actions()
-	velocity = move_and_slide(velocity)
-	if cooldown != 0:
-		cooldown -= 1
+  manage_velocity(delta)
+  manage_actions()
+  velocity = move_and_slide(velocity)
+  if cooldown != 0:
+    cooldown -= 1
 
 
 func manage_velocity(delta):
@@ -43,13 +43,13 @@ func manage_velocity(delta):
     velocity.x = -60
 
 func manage_actions():
-	if Input.is_action_just_pressed("left_mouse_clicked") and rock_counter != 0 && cooldown == 0:
-		var rock_inst = rock.instance()
-		owner.add_child(rock_inst)
-		rock_inst.transform = $aim_point.global_transform
-		rock_inst.starting_position = $aim_point.global_position
-		rock_inst.apply_central_impulse((mouse_position - position).normalized() * 350)
-		rock_inst.starting_direction = (mouse_position - position).normalized()
-		rock_counter -= 1
-		cooldown = 50
+  if Input.is_action_just_pressed("left_mouse_clicked") and rock_counter != 0 && cooldown == 0:
+    var rock_inst = rock.instance()
+    owner.add_child(rock_inst)
+    rock_inst.transform = $aim_point.global_transform
+    rock_inst.starting_position = $aim_point.global_position
+    rock_inst.apply_central_impulse((mouse_position - position).normalized() * 350)
+    rock_inst.starting_direction = (mouse_position - position).normalized()
+    rock_counter -= 1
+    cooldown = 50
 
