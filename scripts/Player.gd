949a1232009291bc3a1f6eabf53662e39148bf81
Macikolej <oiejnik152@gmail.com>
2021-04-04 16:03:47 +0200

    Added throwing bushes

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -6,31 +6,39 @@ extends KinematicBody2D
 # var b = "text"
 var velocity;
 var mouse_position;
+var speed;
+var rock;
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-
-  pass # Replace with function body.
+	rock = preload("../scenes/Rock.tscn")
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	manage_velocity(delta)
-	move_and_collide(velocity)
-
-func _input(event):
-	if event is InputEventMouseMotion:
-		mouse_position = get_local_mouse_position()
-		rotation += mouse_position.angle()
+	manage_actions()
+	velocity = move_and_slide(velocity)
 
 func manage_velocity(delta):
 	velocity = Vector2.ZERO
+	mouse_position = get_local_mouse_position()
+	rotation += mouse_position.angle()
+	
 	if Input.is_action_pressed("move_up"):
-		velocity.y = -60 * delta
+		velocity.y = -60
 	if Input.is_action_pressed("move_down"):
-		velocity.y = 60 * delta
+		velocity.y = 60
 	if Input.is_action_pressed("move_right"):
-		velocity.x = 60 * delta
+		velocity.x = 60
 	if Input.is_action_pressed("move_left"):
-		velocity.x = -60 * delta
+		velocity.x = -60
+
+func manage_actions():
+	if Input.is_action_just_pressed("left_mouse_clicked"):
+		var rock_inst = rock.instance()
+		owner.add_child(rock_inst)
+		rock_inst.transform = $aim_point.global_transform
+		rock_inst.apply_central_impulse(Vector2(200, 0))
+		rock_inst.add_torque(200)
