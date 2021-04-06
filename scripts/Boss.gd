1a617b6a8d827f9762642ec698656db5d3e2d0ae
Wojciech Olejnik <divoolej@gmail.com>
2021-04-06 16:18:55 +0200

    Fix boss & bush

--- scripts/Boss.gd
+++ scripts/Boss.gd
@@ -16,21 +16,22 @@ func _ready():
   instance_id = get_instance_id()
 
 func _process(delta):
-	if player != null:
-		direction = (player.position - position).normalized()
-		rotation = direction.angle() + PI / 2
-	if knockbacked != true:
-		velocity = direction * SPEED
-		velocity = move_and_slide(velocity)
-	else:
-		velocity = direction * -750
-		move_and_slide(velocity)
-		countdown += 1
-		if countdown == 3:
-			knockbacked = false
-			countdown = 0
-  
-	
+  if player != null:
+    direction = (player.position - position).normalized()
+    rotation = direction.angle() + PI / 2
+  if knockbacked != true:
+    velocity = direction * SPEED
+    for i in get_slide_count():
+      if get_slide_collision(i).collider.name.begins_with("Bush"):
+        velocity = 2 * velocity.rotated(PI / 4)
+    velocity = move_and_slide(velocity)
+  else:
+    velocity = direction * -750
+    move_and_slide(velocity)
+    countdown += 1
+    if countdown == 3:
+      knockbacked = false
+      countdown = 0
 
 
 
