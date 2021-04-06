4bfcf98552adef536975b28defa0dc0037a75116
Macikolej <oiejnik152@gmail.com>
2021-04-06 16:30:39 +0200

    Added collectibles

--- scripts/Player.gd
+++ scripts/Player.gd
@@ -11,6 +11,8 @@ var rock;
 var rock_counter;
 var cooldown
 var boss
+var contract_counter
+var cooldown_collectibles = 0
 
 
 # Called when the node enters the scene tree for the first time.
@@ -18,49 +20,57 @@ func _ready():
   rock = preload("../scenes/Rock.tscn")
   boss = preload("../scripts/Boss.gd")
   rock_counter = 3
+  contract_counter = 0
   cooldown = 0
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	manage_velocity(delta)
-	manage_actions()
-	velocity = move_and_slide(velocity)
-	if cooldown != 0:
-		cooldown -= 1
-	check_for_death()
+  manage_velocity(delta)
+  manage_actions()
+  velocity = move_and_slide(velocity)
+  if cooldown != 0:
+    cooldown -= 1
+  if cooldown_collectibles != 0:
+    cooldown_collectibles -= 1
+  check_for_collisions()
 
 
-func check_for_death():
-	for i in get_slide_count():
-		if get_slide_collision(i).collider.name == "Boss":
-			self.queue_free()
+func check_for_collisions():
+  for i in get_slide_count():
+    if get_slide_collision(i).collider.name == "Boss":
+      self.queue_free()
+    if get_slide_collision(i).collider.has_method("collect") and cooldown_collectibles == 0:
+      contract_counter += 1
+      cooldown_collectibles = 2
+      get_slide_collision(i).collider.collect()
+      print(contract_counter)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
 func manage_velocity(delta):
-	velocity = Vector2.ZERO
-	mouse_position = get_global_mouse_position()
-	var vector = mouse_position - position
-	rotation = vector.angle()
+  velocity = Vector2.ZERO
+  mouse_position = get_global_mouse_position()
+  var vector = mouse_position - position
+  rotation = vector.angle()
 
-	if Input.is_action_pressed("move_up"):
-		velocity.y = -60
-	if Input.is_action_pressed("move_down"):
-		velocity.y = 60
-	if Input.is_action_pressed("move_right"):
-		velocity.x = 60
-	if Input.is_action_pressed("move_left"):
-		velocity.x = -60
+  if Input.is_action_pressed("move_up"):
+    velocity.y = -60
+  if Input.is_action_pressed("move_down"):
+    velocity.y = 60
+  if Input.is_action_pressed("move_right"):
+    velocity.x = 60
+  if Input.is_action_pressed("move_left"):
+    velocity.x = -60
 
 func manage_actions():
-	if Input.is_action_just_pressed("left_mouse_clicked") and rock_counter != 0 && cooldown == 0:
-		var rock_inst = rock.instance()
-		owner.add_child(rock_inst)
-		rock_inst.transform = $aim_point.global_transform
-		rock_inst.starting_position = $aim_point.global_position
-		rock_inst.apply_central_impulse((mouse_position - position).normalized() * 350)
-		rock_inst.starting_direction = (mouse_position - position).normalized()
-		rock_counter -= 1
-		cooldown = 50
+  if Input.is_action_just_pressed("left_mouse_clicked") and rock_counter != 0 && cooldown == 0:
+    var rock_inst = rock.instance()
+    owner.add_child(rock_inst)
+    rock_inst.transform = $aim_point.global_transform
+    rock_inst.starting_position = $aim_point.global_position
+    rock_inst.apply_central_impulse((mouse_position - position).normalized() * 350)
+    rock_inst.starting_direction = (mouse_position - position).normalized()
+    rock_counter -= 1
+    cooldown = 50
 
--- scripts/Rock.gd
+++ scripts/Rock.gd
@@ -9,23 +9,23 @@ var starting_direction
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	gravity_scale = 0
-	is_rolling = false
-	contact_monitor = true
-	contacts_reported = 100
+  gravity_scale = 0
+  is_rolling = false
+  contact_monitor = true
+  contacts_reported = 100
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	if (((position - starting_position).length() > 70) and is_rolling == false):
-		is_rolling = true
-		add_central_force(-starting_direction * 500)
-	if (abs((starting_direction - linear_velocity.normalized()).length()) > 0.0001 and is_rolling == true):
-		set_applied_force(Vector2(0,0))
-		set_linear_velocity(Vector2(0,0))
+  if (((position - starting_position).length() > 70) and is_rolling == false):
+    is_rolling = true
+    add_central_force(-starting_direction * 500)
+  if (abs((starting_direction - linear_velocity.normalized()).length()) > 0.0001 and is_rolling == true):
+    set_applied_force(Vector2(0,0))
+    set_linear_velocity(Vector2(0,0))
 
 func _on_Node2D_body_entered(body):
-	if body.get_name() == "Player":
-		body.rock_counter += 1
-		self.queue_free()
-	if body.get_name() == "Boss" && is_rolling != true:
-		body.knockbacked = true
+  if body.get_name() == "Player":
+    body.rock_counter += 1
+    self.queue_free()
+  if body.get_name() == "Boss" && is_rolling != true:
+    body.knockbacked = true
