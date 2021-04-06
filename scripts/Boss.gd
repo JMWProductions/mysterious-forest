2cd97f5b07eb0a53d3d50fee671c4420016a60ea
Wojciech Olejnik <divoolej@gmail.com>
2021-04-06 15:55:37 +0200

    Fix Roses

--- scripts/Boss.gd
+++ scripts/Boss.gd
@@ -9,27 +9,27 @@ var instance_id
 const SPEED = 33.0
 
 func _ready():
-	player = get_tree().get_root().get_node("World").get_node("Player")
-	knockbacked = false
-	countdown = 0
-	instance_id = get_instance_id()
+  player = get_tree().get_root().get_node("World").get_node("Player")
+  knockbacked = false
+  countdown = 0
+  instance_id = get_instance_id()
 
 func _process(delta):
-	if player != null:
-		var direction = (player.position - position).normalized()
-		rotation = direction.angle() + PI / 2
-		if knockbacked != true:
-			velocity = direction * SPEED
-			velocity = move_and_slide(velocity)
-		else:
-			velocity = direction * -750
-			move_and_slide(velocity)
-			countdown += 1
-			if countdown == 3:
-				knockbacked = false
-				countdown = 0
-	
-		
+  if player != null:
+    var direction = (player.position - position).normalized()
+    rotation = direction.angle() + PI / 2
+    if knockbacked != true:
+      velocity = direction * SPEED
+      velocity = move_and_slide(velocity)
+    else:
+      velocity = direction * -750
+      move_and_slide(velocity)
+      countdown += 1
+      if countdown == 3:
+        knockbacked = false
+        countdown = 0
+  
+    
 
 
 
